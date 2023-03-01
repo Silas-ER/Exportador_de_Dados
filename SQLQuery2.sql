87ade49c31f73b0a276841075db285dd3b158861
Silas Eduardo <90927889+SilasEd@users.noreply.github.com>
2023-03-01 08:24:11 -0300

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,18 +10,18 @@ GO
 
 CREATE VIEW [dbo].[vwEntradasG] AS
 SELECT MOVIMENTO.Chave_fato,
-	MOVIMENTO.Cod_rotina, 
-	MOVIMENTO.Cod_ccustoD, 
-	MOVIMENTO.Data_lancto, 
-	MOVIMENTO.Num_docto_aux, 
-	MOVIMENTO.valor_dc,
-	MOVIMENTO.Tipo_partida,
-	NOMECUSTO.Cod_ccusto,
-	NOMECUSTO.Nome_ccusto,
-	ENTRADAS.Cod_cli_for,
-	ENTRADAS.Cod_tipo_mv,
-	NOMECDA.Nome_cadastro,
-	TIPORATEIO.Desc_Rateio
+       MOVIMENTO.Cod_rotina, 
+       MOVIMENTO.Cod_ccustoD, 
+       MOVIMENTO.Data_lancto, 
+       MOVIMENTO.Num_docto_aux, 
+       MOVIMENTO.valor_dc,
+       MOVIMENTO.Tipo_partida,
+       NOMECUSTO.Cod_ccusto,
+       NOMECUSTO.Nome_ccusto,
+       ENTRADAS.Cod_cli_for,
+       ENTRADAS.Cod_tipo_mv,
+       NOMECDA.Nome_cadastro,
+       TIPORATEIO.Desc_Rateio
 FROM 
 vwMovto MOVIMENTO
 INNER JOIN tbEntradas ENTRADAS ON (MOVIMENTO.Chave_fato = ENTRADAS.Chave_fato)
